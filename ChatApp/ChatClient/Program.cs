18dfca118a4a0fcda84ea9c8fa8fe9a075ad7eb2
Sebastian Gonzalo Alvarez Concepcion <sebastianalvarezconcepcion@gmail.com>
2025-05-30 01:09:28 -0400

    Uploading: ChatApp

---
+++ ChatApp/ChatClient/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Type your name: ");
+            string? name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("You can't connect to the server because you sucks");
+                return;
+            }
+            TcpClient client = new TcpClient("127.0.0.1", 13000);
+
+            Console.WriteLine("The connection was success");
+
+            NetworkStream clientStream = client.GetStream();
+            byte[] buffer = Encoding.UTF8.GetBytes(name);
+            await clientStream.WriteAsync(buffer, 0, buffer.Length);
+
+            byte[] buffer2 = new byte[1024];
+            int bu = await clientStream.ReadAsync(buffer2, 0, buffer2.Length);
+            string welcomeMessage = Encoding.UTF8.GetString(buffer2, 0, bu);
+            Console.WriteLine(welcomeMessage);
+
+
+            var broadcastTask = Task.Run(() => BroadcastListenerMessage(client));
+
+            while (true)
+            {
+                Console.Write("You> ");
+                string message = Console.ReadLine();
+                string messageWithName = $"{message}";
+                if (message == "Exit") break;
+
+                byte[] buffer3 = Encoding.UTF8.GetBytes(messageWithName);
+                await clientStream.WriteAsync(buffer3, 0, buffer3.Length);
+            }
+            await broadcastTask;
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    static async Task BroadcastListenerMessage(TcpClient client)
+    {
+        try
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                int bytes = await stream.ReadAsync(buffer, 0, buffer.Length);
+                if (bytes == 0)
+                {
+                    // El servidor cerró la conexión
+                    Console.WriteLine("Disconnected from server.");
+                    break;
+                }
+                string message = Encoding.UTF8.GetString(buffer, 0, bytes);
+                Console.WriteLine(message);
+                Console.Write("You> ");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ ChatApp/ChatServer/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+
+class Program
+{
+    static List<TcpClient> tcpClients= new List<TcpClient>();
+    static readonly object clientLocks = new object();
+    static async Task Main(string[] args)
+    {
+        int port = 13000;
+        TcpListener tcpListener = new TcpListener(IPAddress.Any, port);
+        tcpListener.Start();
+
+        Console.WriteLine($"Listening in iP: {IPAddress.Any} and port: {port}");
+        while (true)
+        {
+            TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+            lock (clientLocks)
+            {
+                tcpClients.Add(tcpClient);
+            }
+            _ = Task.Run(() => HandleClientAsync(tcpClient));
+        }
+    }
+
+    static async Task BreadCastMessageAsync(string message, TcpClient excludedClient)
+    {
+        Byte[] buffer = Encoding.UTF8.GetBytes(message);
+
+        List<TcpClient> tcpClientsCopy;
+
+        lock (clientLocks)
+        {
+            tcpClientsCopy = tcpClients.ToList();
+        }
+
+        foreach (TcpClient tcpClient in tcpClientsCopy)
+        {
+            if (tcpClient == excludedClient) continue;
+
+            try
+            {
+                NetworkStream stream = tcpClient.GetStream();
+                await stream.WriteAsync(buffer, 0, buffer.Length); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+    }
+
+    public static async Task HandleClientAsync(TcpClient tcpClient)
+    {
+        try
+        {
+            NetworkStream stream = tcpClient.GetStream();
+            byte[] buffer = new byte[1024];
+            int byteRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string name = Encoding.UTF8.GetString(buffer, 0, byteRead);
+
+
+            byte[] welcomeData = Encoding.UTF8.GetBytes($"Welcome {name}, you are successfully connected");
+            await stream.WriteAsync(welcomeData, 0, welcomeData.Length);
+
+            IPEndPoint? remoteEndpoint = tcpClient.Client.RemoteEndPoint as IPEndPoint;
+            string ip = remoteEndpoint?.Address.ToString();
+            Console.WriteLine($"The user {name} is connected from ip {ip}");
+
+            while (true)
+            {
+                byte[] messageBytes = new byte[1024];
+                int byteR = await stream.ReadAsync(messageBytes, 0, messageBytes.Length);
+
+                if (byteR == 0)
+                {
+                    lock (clientLocks)
+                    {
+                        tcpClients.Remove(tcpClient);
+                    }
+                    tcpClient.Close();
+                    Console.WriteLine($"{name} disconnected.");
+                    break;
+                }
+
+                string message = Encoding.UTF8.GetString(messageBytes, 0, byteR);
+                if (!string.IsNullOrWhiteSpace(message))
+                {
+                    Console.WriteLine($"{name}> {message} ");
+
+                    await BreadCastMessageAsync($"\n{name}> {message}", tcpClient);
+                }
+            }
+            Console.WriteLine("Client disconnected");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+
+}
